d9971ca613cca40c812ad1ee9e65226450b75ebb
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-06-15 16:56:10 +0600

    doing

--- Php basic/01.php
+++ Php basic/01.php
@@ -26,3 +26,19 @@ echo '$name';
 echo '{$name}';
 echo "{$name}";
 echo " <br> \n+welcome for ". $name;
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <h1>hey this h1</h1>
+    <p> this is my name <?="$name"?></p>
+</body>
+</html>
---
+++ Php basic/02.php
@@ -0,0 +1,19 @@
+<?php
+
+define('name','abrar',0);
+
+echo defined("name");
+
+const work = "done";
+echo work;
+
+$paid = 'paid';
+
+define("look".$paid,$paid);
+
+echo "<br> " .lookpaid;
+
+$app = "fb";
+$$app = "facebook";
+
+echo "$fb {$app}";
