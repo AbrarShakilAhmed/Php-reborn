0ae3bc15450b66d1be1571890f2990455ce2e550
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-06-15 16:37:44 +0600

    doing

--- Php basic/01.php
+++ Php basic/01.php
@@ -1,8 +1,28 @@
 <?php
 
-echo "hello world <br>";
+echo ("hello world <br>");
 
 print " hello word 2 <br>";
 
 echo print "world";
 //  print is always return 1
+
+echo  "\n   \to hey";
+
+// concatenation in php
+
+echo " hello world"," old world" ;
+echo " hello world"." old world" ;
+$x = 3;
+// $y = &$x; refference cng also
+$x =343;
+
+echo $x;
+
+$name  =  "abrar";
+
+echo "$name";
+echo '$name';
+echo '{$name}';
+echo "{$name}";
+echo " <br> \n+welcome for ". $name;
