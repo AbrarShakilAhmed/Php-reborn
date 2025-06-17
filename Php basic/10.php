d3ed03498313b37f4bd00318f44317a3d9a3ab42
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-06-17 18:00:15 +0600

    doing

---
+++ Php basic/10.php
@@ -0,0 +1,58 @@
+<?php
+// $arr = [1,3,43,535,535,3,53,2,42,4324];
+// foreach($arr as$key => $sing){
+//   echo"\n".$key ." ". $sing;
+// }
+// for($i=0;$i<count($arr);$i++){
+//   echo $arr[$i];
+// }
+
+// $arr = [
+//   "name" => "Abrar",
+//   "age" => "20",
+//   "occ" => ["dev","tutor"],
+//   "skill" => ["js","ts","php"]
+// ];
+
+// foreach($arr as $key =>&$single){
+//   if(is_array($single)){
+//     foreach($single as $tiny){
+//   echo $key . " : " .$tiny. "\n";
+
+
+//     }
+//   }else{
+//   echo $key . " : " .$single. "\n";
+
+//   }
+// }
+
+$day = 7;
+
+// switch($day){
+//   case 1:
+//     echo "Satarday";
+//     break;
+//   case 2:
+//     echo "sunday";
+//     break;
+//   case 3:
+//     echo "monday";
+//     break;
+//   case 4:
+//     echo "twesday";
+//     break;
+//   case 5:
+//     echo "wednesday";
+//     break;
+//   case 6:
+//     echo "thursday";
+//     break;
+//   case 7:
+//     echo "Friday";
+//     break;
+//   default :
+//     echo "try a valid day";
+// }
+
+?>
--- temp.js
+++ temp.js
@@ -1,3 +1,15 @@
-let x = 'hello'
-x = x + ' world'
-console.log(null + 1)
+function x() {
+  setTimeout(() => {
+    return 1
+  }, 3000)
+}
+
+x = x()
+console.log(x)
+
+if (x == 1) {
+  console.log(1)
+} else if (x == 2) {
+  console.log(2)
+}
+
