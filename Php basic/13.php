835eab52f5983c379c39e03dbeeb777148d420a6
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-06-18 17:02:44 +0600

    doing

--- Php basic/13.php
+++ Php basic/13.php
@@ -1,10 +1,10 @@
 <?php
-
+$user = "abrar shakil";
 $demo = <<<Html
 
 <div>
 <h1 style='color:skyblue;'>
-This is the heading from heredoc
+This is the heading from heredoc and user is $user
 </h1>
 </div>
 
