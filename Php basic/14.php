cbbbd621c03ad54e3bdc9f59c3873d9eeefe542e
Abrarshakilahmed <developerabrarshakil@gmail.com>
2025-06-19 21:53:52 -0700

    doing

--- Php basic/14.php
+++ Php basic/14.php
@@ -1,7 +1,29 @@
 <?php
 
-$arr = [1,2,3,4,5,6,7,8];
-$arr2 = ['a','b','c',0];
+$a = array(
+  array(
+    'id' => 5698,
+    'first_name' => 'Peter',
+    'last_name' => 'Griffin',
+  ),
+  array(
+    'id' => 4767,
+    'first_name' => 'Ben',
+    'last_name' => 'Smith',
+  ),
+  array(
+    'id' => 3809,
+    'first_name' => 'Joe',
+    'last_name' => 'Doe',
+  ),[
+    'id' => 3809,
+    'first_name' => 'Joe',
+    'last_name' => ['hacker',
+    'id' => 3809,
+    'first_name' => 'Joe',
+    'last_name' => 'Doe',],
+  ]
+);
 
-print_r( array_map(fn($e)=>$e,$arr));
-print_r($arr);
+$last_names = array_column($a, 'last_name');
+print_r($last_names);
